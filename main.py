c27f52ccd6560f21e184a178dd7be6c7233527ce
DianaBuliga <61600528+DianaBuliga@users.noreply.github.com>
2021-12-17 16:23:17 +0200

    Butoane create

--- main.py
+++ main.py
@@ -2,31 +2,24 @@ from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
-        QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget)
-from PyQt5.QtWidgets import QMainWindow,QWidget, QPushButton, QAction
+                             QSizePolicy, QSlider, QStyle, QVBoxLayout, QComboBox)
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QAction
 from PyQt5.QtGui import QIcon
 import sys
+
 
 class VideoWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super(VideoWindow, self).__init__(parent)
         self.setWindowTitle("PyQT Project")
+        self.setStyleSheet("background-image: url(Artboard_85.png);")
 
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.setAcceptDrops(True)
         videoWidget = QVideoWidget()
 
-        self.playButton = QPushButton()
-        self.playButton.setEnabled(False)
-        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-        self.playButton.clicked.connect(self.play)
-
-        self.positionSlider = QSlider(Qt.Horizontal)
-        self.positionSlider.setRange(0, 0)
-        self.positionSlider.sliderMoved.connect(self.setPosition)
-
-        #self.
+        #self.i
 
         self.errorLabel = QLabel()
         self.errorLabel.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Maximum)
@@ -37,6 +30,12 @@ class VideoWindow(QMainWindow):
         openAction.setStatusTip('Open movie')
         openAction.triggered.connect(self.openFile)
 
+        # Create save action
+        saveAction = QAction(QIcon('save.png'), '&Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.setStatusTip('Save File')
+        saveAction.triggered.connect(self.saveFile)
+
         # Create exit action
         exitAction = QAction(QIcon('exit.png'), '&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
@@ -46,26 +45,64 @@ class VideoWindow(QMainWindow):
         # Create menu bar and add action
         menuBar = self.menuBar()
         fileMenu = menuBar.addMenu('&File')
-        exitMenu = menuBar.addMenu('&Exit')
+        # saveMenu = menuBar.addMenu('&Save')
+        # exitMenu = menuBar.addMenu('&Exit')
         #fileMenu.addAction(newAction)
         fileMenu.addAction(openAction)
+        fileMenu.addAction(saveAction)
         fileMenu.addAction(exitAction)
 
-        exitMenu.addAction(exitAction)
+        # exitMenu.addAction(exitAction)
 
         # Create a widget for window contents
         wid = QWidget(self)
         self.setCentralWidget(wid)
 
+        #Create position slider layout
+        sliderLayout = QHBoxLayout()
+        sliderLayout.setContentsMargins(0, 0, 0, 0)
+
+        # position slider create
+        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+        sliderLayout.addWidget(self.positionSlider)
+
+        # play button create
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+        # speed play create
+        self.comboSpeed = QComboBox()
+        self.comboSpeed.addItems(["x0.5", "x1", "x2"])
+        self.comboSpeed.setEnabled(False)
+        self.comboSpeed.setGeometry(10, 10, 10, 10)
+
+        # cut/done button create
+        self.cutButton = QPushButton()
+        self.cutButton.setEnabled(False)
+        self.cutButton.setText("Cut")
+
+        #save button create
+        self.saveButton = QPushButton()
+        self.saveButton.setEnabled(False)
+        self.saveButton.setText("Save")
+
         # Create layouts to place inside widget
         controlLayout = QHBoxLayout()
         controlLayout.setContentsMargins(0, 0, 0, 0)
         controlLayout.addWidget(self.playButton)
-        controlLayout.addWidget(self.positionSlider)
-#        controlLayout.addWidget(self.)
+        # controlLayout.addWidget(self.positionSlider)
+        # controlLayout.addWidget(self.)
+        controlLayout.addWidget(self.comboSpeed)
+        controlLayout.addWidget(self.cutButton)
+        controlLayout.addWidget(self.saveButton)
 
         layout = QVBoxLayout()
         layout.addWidget(videoWidget)
+        layout.addLayout(sliderLayout)
         layout.addLayout(controlLayout)
         layout.addWidget(self.errorLabel)
 
@@ -86,6 +123,14 @@ class VideoWindow(QMainWindow):
             self.mediaPlayer.setMedia(
                     QMediaContent(QUrl.fromLocalFile(fileName)))
             self.playButton.setEnabled(True)
+            self.comboSpeed.setEnabled(True)
+            self.cutButton.setEnabled(True)
+
+    def saveFile(self):
+        file = open(QFileDialog.getSaveFileName(self, 'Save File'))
+        text = self.textEdit.toPlainText()
+        file.write(text)
+        file.close()
 
     def exitCall(self):
         sys.exit(app.exec_())
